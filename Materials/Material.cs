c4473a6009a53db8b32a94a03282d7d0935963a4
Xhack <72313282+energykid@users.noreply.github.com>
2025-06-20 23:13:07 -0400

    Material basics

---
+++ Materials/Material.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Weaponology.Materials
+{
+    public abstract class Material
+    {
+        public string Name = "";
+        public int ItemType;
+        public Types.MaterialClass Class;
+        public string GradientFile = "";
+        public Material(string name, int item, Types.MaterialClass type) {
+            Name = Name;
+            ItemType = item;
+            Class = type;
+        }
+        public void SetShader()
+        {
+            WayLib.Graphics.Gradients.SetCurrentGradient(GradientFile, 0);
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, null, null, WayLib.WayLib.GradientShader, Main.GameViewMatrix.ZoomMatrix);
+        }
+        public void ResetShader()
+        {
+            Main.spriteBatch.End(); 
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+        }
+    }
+
+    public class Types
+    {
+        public enum MaterialClass
+        {
+            Metal,
+            Gem,
+            Wood,
+            Magic
+        }
+
+        public enum WeaponClass
+        {
+            Blade,
+            Polearm,
+            Tool,
+            Curved
+        }
+    }
+}
--- Weaponology.cs
+++ Weaponology.cs
@@ -7,7 +7,6 @@ using Terraria.ModLoader;
 
 namespace Weaponology
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class Weaponology : Mod
 	{
 
